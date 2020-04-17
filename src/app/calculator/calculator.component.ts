b7fe0abd2e39b18f10234fa755446874ae76575a
Abdulhussain Songerwala <abdulhussainsongerwala@Abdulhussains-MacBook-Pro.local>
2020-04-17 13:58:40 +0530

    Basic calculations

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  resultString = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  read = (input) => this.resultString += input;
+  clear = () => this.resultString = '';
+  backspace = () => this.resultString = this.resultString.substring(0, this.resultString.length - 1);
+  execute() {
+    const sections = this.resultString.split(/(\+|-|\*|\/)/);
+    const expression = parseFloat(sections[0]) + sections[1] + parseFloat(sections[2]);
+    const resultDecimal = eval(expression);
+    this.resultString = resultDecimal.toString();
+  }
 }
